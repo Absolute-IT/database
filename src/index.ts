f16bdc5be7111a675d711bd8c05d174b7542821b
Zei33 <zei33@users.noreply.github.com>
2024-04-10 22:07:40 +1000

    Updated to a src/dist layout and upgraded the typescript.

--- src/index.ts
+++ src/index.ts
@@ -22,25 +22,19 @@ class Database {
 			// Can't use lambda functions on the results because OkPacket is a potential return type.
 			if ("length" in results) {
 				let final: any[] = [];
-				// Results is an array.
 				for (const result of results as OkPacket[] | RowDataPacket[]) {
 					if ((result as OkPacket).insertId !== undefined) {
-						// This is an OkPacket.
 						final.push(result as any);
 					} else {
-						// This is a RowDataPacket.
 						final.push(result as any[]);
 					}
 				}
 
 				return final;
 			} else {
-				// Results is not an array.
 				if ((results as OkPacket).insertId !== undefined) {
-					// This is an OkPacket.
 					return [results] as any[];
 				} else {
-					// This is a RowDataPacket.
 					return results as unknown as any[];
 				}
 			}
